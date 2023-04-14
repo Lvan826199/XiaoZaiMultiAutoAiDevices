68cd73860c808f9003bbda89cc6cb0c9e863e3ba
van_liu <826199528@qq.com>
2023-04-14 15:27:30 +0800

    增加日志图片清理功能

--- start.py
+++ start.py
@@ -11,7 +11,14 @@ from utils import index
 
 
 def start():
+    deleteUtilslog()
     index.main()
+
+def deleteUtilslog():
+    try:
+        shutil.rmtree("utils/log")
+    except:
+        pass
 
 # 从根目录启动，确保相对路径调用正常
 if __name__ == '__main__':
